91332c18a9471df46223d0383ddd4962f22d1a85
Specktreon <lezzar37@gmail.com>
2025-05-30 20:57:00 +0200

    .

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 // import { NextPageContext } from "next";
 // import { getSession, signIn } from "next-auth/react";
-import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { signIn } from "next-auth/react";
@@ -27,8 +26,6 @@ import Input from "@/components/Input";
 // }
 
 const Auth = () => {
-  const router = useRouter();
-
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -46,15 +43,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -115,14 +109,14 @@ const Auth = () => {
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               {/* Google OAuth Login */}
               <div
-                onClick={() => signIn("google", { callbackUrl: "/" })}
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={32} />
               </div>
               {/* GitHub OAuth Login */}
               <div
-                onClick={() => signIn("github", { callbackUrl: "/" })}
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={32} />
