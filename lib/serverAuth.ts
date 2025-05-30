cfb8a6fff3f970d290178c9b76204e0f2acad7fd
Specktreon <lezzar37@gmail.com>
2025-05-30 16:27:27 +0200

    current user

---
+++ lib/serverAuth.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import prismadb from "@/lib/prismadb";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session?.user?.email) {
+    throw new Error("Not signed in");
+  }
+
