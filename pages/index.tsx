569b8d63667143b95788b829b85dc609e8375c08
Specktreon <lezzar37@gmail.com>
2025-05-30 16:28:04 +0200

    adding user session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="inline-block mt-5 border-solid border-4 border-red-500 p-4 text-green-500 text-2xl">
-        Netflix Clone
+        Netflix Clone 1600
       </h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className="h-18 w-full bg-white" onClick={() => signOut()}>
+        Log out
+      </button>
     </>
   );
 }
