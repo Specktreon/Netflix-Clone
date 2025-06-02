87d263b7766b0de324b504ca0566a40d9e93c668
Specktreon <lezzar37@gmail.com>
2025-06-02 22:37:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { getSession, signOut } from "next-auth/react";
 import { NextPageContext } from "next";
-
-import useCurrentUser from "@/hooks/useCurrentUser";
+import { getSession } from "next-auth/react";
+import Navbar from "@/components/Navbar";
+// import Billboard from "@/components/Billboard";
+// import MovieList from "@/components/MovieList";
+// import InfoModal from "@/components/InfoModal";
+// import useMovieList from "@/hooks/useMovieList";
+// import useFavorites from "@/hooks/useFavorites";
+// import useInfoModal from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
+  // Check if the session does not exists
   if (!session) {
     return {
       redirect: {
@@ -14,23 +20,26 @@ export async function getServerSideProps(context: NextPageContext) {
       },
     };
   }
+
   return {
     props: {},
   };
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  // const { data: movies = [] } = useMovieList();
+  // const { data: favorites = [] } = useFavorites();
+  // const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
-      <h1 className="inline-block mt-5 border-solid border-4 border-red-500 p-4 text-green-500 text-2xl">
-        Netflix Clone 1600
-      </h1>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button className="h-18 w-full bg-white" onClick={() => signOut()}>
-        Log out
-      </button>
+      {/* <InfoModal visible={isOpen} onClose={closeModal} /> */}
+      <Navbar />
+      {/* <Billboard/>
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+        <MovieList title="My List" data={favorites}/>
+      </div> */}
     </>
   );
 }
